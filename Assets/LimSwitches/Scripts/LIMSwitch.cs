69378183b541b47b81b45802383377fbdab8334f
Jaeseong Lim <dlawotjd1020@gmail.com>
2019-10-10 18:18:15 +0900

    #3 Switch 의 색상 변경 기능
    스위치 온 오프 시 스위치 색상변경 완료

--- Assets/LimSwitches/Scripts/LIMSwitch.cs
+++ Assets/LimSwitches/Scripts/LIMSwitch.cs
@@ -9,7 +9,11 @@ public class LIMSwitch : MonoBehaviour
     public bool isOn;
     // 0 ~ 3 속도 조절을 할 수 있는 스크롤 생성, 스위치의 이동시간 (최대 3)
     [Range(0,3)]
-    public float moveDuration = 3f;  
+    public float moveDuration = 3f;
+    //컬러 기본값
+    public Color handleColor = Color.white;
+    public Color onSwitchColor = Color.green;
+    public Color offSwitchColor = Color.gray; 
 
     const float totalHandleMoveLength = 72;    //좌 우 최대 이동 할 수 있는 거리
     const float halfMoveLength = totalHandleMoveLength / 2;  //최대 이동거리의 절반 ?
@@ -20,14 +24,22 @@ public class LIMSwitch : MonoBehaviour
 
     //코루틴 함수
     Coroutine moveHandleCoroutine;      // 핸들 이동 코루틴
+    Coroutine backgroundColorChangeCoroutine; //배경색 변경 코루틴
     
     // Start is called before the first frame update
     void Start()
     {
         // Handle 오브젝트 초기화 
         GameObject handleObfect = transform.Find("Handle").gameObject;
-
         handleRectTransform = handleObfect.GetComponent<RectTransform>();
+        //핸들 이미지
+        handleImage = GetComponent<Image>();
+        handleImage.color = handleColor;
+
+        //배경 이미지
+        backgroundImage = GetComponent<Image>();
+        backgroundImage.color = offSwitchColor;
+
         if (isOn)
         {
             handleRectTransform.anchoredPosition = new Vector2(halfMoveLength ,0);
@@ -50,12 +62,25 @@ public class LIMSwitch : MonoBehaviour
         float ratio = Mathf.Abs(dis.x) / totalHandleMoveLength;
         float duration = moveDuration * ratio;
 
+        //handlemoveCoroutine
         if (moveHandleCoroutine != null)
         {
             StopCoroutine(moveHandleCoroutine);
             moveHandleCoroutine = null;
         }
-        moveHandleCoroutine = StartCoroutine(MoveHandle(fromPos,toPos,duration));
+        moveHandleCoroutine = 
+            StartCoroutine(MoveHandle(fromPos,toPos,duration));
+
+        //backgruondChangeColorCouroutine
+        Color fromColor = backgroundImage.color;
+        Color toColor = (isOn) ? onSwitchColor : offSwitchColor;
+        if (backgroundColorChangeCoroutine != null)
+        {
+            StopCoroutine(backgroundColorChangeCoroutine);
+            backgroundColorChangeCoroutine = null;
+        }
+        backgroundColorChangeCoroutine = 
+            StartCoroutine(changeBackgroundColor(fromColor,toColor,duration));
     }
     /// <summary>
     /// 핸들을 이동하기 위한 함수 
@@ -72,6 +97,26 @@ public class LIMSwitch : MonoBehaviour
             float t = currTime / duartion;
             Vector2 newPos = Vector2.Lerp(fromPos, toPos, t);
             handleRectTransform.anchoredPosition = newPos;
+
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+    /// <summary>
+    /// 스위치 배경색 변경 하는 코루틴 함수
+    /// </summary>
+    /// <param name="fromColor">처음 시작 색상</param>
+    /// <param name="toColor">변경될 색상</param>
+    /// <param name="duration">변경되는 시간</param>
+    /// <returns>없음</returns>
+    IEnumerator changeBackgroundColor(Color fromColor, Color toColor, float duration)
+    {
+        float currTime = 0f;
+        while (currTime < duration)
+        {
+            float t = currTime / duration;
+            Color newColor = Color.Lerp(fromColor, toColor, t);
+            backgroundImage.color = newColor;
 
             currTime += Time.deltaTime;
             yield return null;
